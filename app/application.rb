db45e96a1176e1545d8d188da396a41950752040
Michael Carroll <carrollm2@gmail.com>
2020-01-24 01:41:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -25,6 +25,13 @@ class Application
       binding.pry
       search_term = req.params["item"]
 
+      if @@items.include?(search_term)
+        @@cart << search_term
+        resp.write "added #{search_term}"
+      else
+        resp.write "We don't have that item"
+      end
+
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
